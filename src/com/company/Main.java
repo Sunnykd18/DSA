c74118e85c4014b5b5a4ae3d72ae434637212f5e
Sunnykd18 <95135405+Sunnykd18@users.noreply.github.com>
2021-12-08 22:23:48 +0530

    "initial commit"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -25,12 +25,69 @@ public class Main {
 
 
 
-        Scanner scn = new Scanner(System.in);
-        int n = scn.nextInt();
-        int d = scn.nextInt();
-        int f = getDigitFrequency(n, d);
-        System.out.println(f);
-    }
+//        Scanner scn = new Scanner(System.in);
+//        int n = scn.nextInt();
+//        int d = scn.nextInt();
+//        int f = getValueInBase(n, d);
+//        System.out.println(f);
+//    }
+//    public static int getValueInBase(int n, int d){
+//        int rv = 0;
+//        int p = 1;
+//        while(n>0){
+//            int dig = n % d;
+//            n = n / d;
+//
+//            rv += dig * p;
+//            p = p * 10;
+//        }
+//        return rv;
+
+
+
+//        Scanner scn = new Scanner(System.in);
+//        int n = scn.nextInt();
+//        int b = scn.nextInt();
+//        int f = getValueInDecimal(n, b);
+//        System.out.println(f);
+//    }
+//    public static int getValueInDecimal(int n, int b){
+//        int rv = 0;
+//        int p = 1;
+//
+//        while(n > 0){
+//            int dig = n % 10;
+//            n = n / 10;
+//
+//            rv += dig * p;
+//            p = p * b;
+//
+//        }
+//        return rv;
+
+//
+//        Scanner scn = new Scanner(System.in);
+//        int n = scn.nextInt();
+//        int [] arr = new int [n];
+//
+//        for(int i = 0; i < arr.length; i++){
+//            arr[i] = scn.nextInt();
+//        }
+//        int max = arr[0];
+//        int min = arr[0];
+//        for(int i = 0; i < arr.length; i++){
+//            if (arr[i]>max){
+//                max = arr[i];
+//            }
+//            if (arr[i]< min){
+//                min = arr[i];
+//            }
+//
+//        }
+//        int span = max - min;
+//        System.out.println(span);
+
+
 
     }
 
---
+++ src/com/company/MaxNum.java
@@ -0,0 +1,33 @@
+package com.company;
+
+
+
+//import java.util.Arrays;
+//import java.util.Collections;
+
+
+
+import java.util.Scanner;
+
+public class MaxNum {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int [] arr = new int [n];
+
+
+         for (int i = 0; i < arr.length;i++){
+             arr[i] = scn.nextInt();
+         }
+         int max = arr[0];
+         for (int i = 1;i< arr.length; i++){
+             if (arr[i] > max) {
+                 max = arr[i];
+             }
+         }
+        System.out.println(max);
+
+    }
+
+}
+
