96063f493e5d8209c1e1873422b3f7387accbe85
Sunnykd18 <95135405+Sunnykd18@users.noreply.github.com>
2021-12-11 18:37:44 +0530

    Next greater Element

---
+++ src/com/company/NGE.java
@@ -0,0 +1,53 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class NGE {
+    public static void display(int[] a){
+        StringBuilder sb = new StringBuilder();
+
+        for(int val: a){
+            sb.append(val + "\n");
+        }
+        System.out.println(sb);
+    }
+    public static void  main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+        for(int i = 0; i< n; i++){
+            a[i] = Integer.parseInt(br.readLine());
+
+        }
+
+        int[] nge = solve(a);
+        display(nge);
+
+    }
+
+    public static int[] solve(int[] arr){
+        int[] ge = new int[arr.length];
+
+        Stack<Integer> st = new Stack<>();
+
+        st.push(arr[arr.length -1]);
+        ge[arr.length - 1] = -1;
+        for (int i = arr.length -2 ; i>= 0 ; i--){
+//           -a+
+            while(st.size() > 0 && arr[i] >= st.peek()){
+                st.pop();
+            }
+            if(st.size() == 0){
+                ge[i] = st.peek();
+
+            }else {
+                ge[i] = st.peek();
+            }
+            st.push(arr[i]);
+        }
+        return ge;
+    }
+}
