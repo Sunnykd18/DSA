7057a845e2f2d0925f39162c23606483408cf8ea
Sunnykd18 <95135405+Sunnykd18@users.noreply.github.com>
2022-02-20 22:04:43 +0530

    BS/Insertion

---
+++ src/Binary_Search_Tree/Insertion.java
@@ -0,0 +1,20 @@
+package Binary_Search_Tree;
+
+public class Insertion {
+    public static void main(String[]args) {
+        int[] a = {8, 4, 1, 5, 9, 2};
+
+        int n = a.length;
+        for (int i = 1; i < n; i++) {
+            int temp = a[i];
+            int j = i - 1;
+
+            while (j >= 0 && a[j] > temp) {
+                a[j + 1] = a[j];
+                j--;
+            }
+            a[j + 1] = temp;
+        }
+
+    }
+}
