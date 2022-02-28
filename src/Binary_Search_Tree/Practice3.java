34c3b5aed510948328ec8efd1f2100e11cc46470
Sunnykd18 <95135405+Sunnykd18@users.noreply.github.com>
2022-02-28 23:40:24 +0530

    Revision

---
+++ src/Binary_Search_Tree/Practice3.java
@@ -0,0 +1,27 @@
+package Binary_Search_Tree;
+
+public class Practice3 {    public static void main(String[] args) {
+    int[] a = {4, 3, 7, 1, 5};
+
+    int n = a.length;
+    for (int i = 0; i < n; i++) {
+        boolean swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (a[i] < a[j]) {
+                swapped = true;
+                Swap(a, j + 1, j);
+            }
+        }
+        if (!swapped){
+            break;
+        }
+
+    }
+}
+    static void Swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+
+    }
+}
